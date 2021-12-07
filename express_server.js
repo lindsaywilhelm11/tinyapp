0506bc93be432fe62a6af8e8f86fe0139905dc1a
Lindsay Wilhelm <lindsaywilhelm11@gmail.com>
2021-12-07 18:41:07 +0000

    updated tiny aoo

--- express_server.js
+++ express_server.js
@@ -31,20 +31,26 @@ app.get("/urls.json", (req, res) => {
   });
 
   app.get("/urls/:shortURL", (req, res) => {
-    const templateVars = { shortURL: generateRandomString(), longURL: req.params.longURL};
+    const templateVars = { shortURL: generateRandomString(), longURL: urlDatabase[req.params.shortURL] };
     res.render("urls_show", templateVars);
     
   });  
 
   app.get("/u/:shortURL", (req, res) => {
-      const longURL = { longURL: req.params.longURL }
-    res.redirect(longURL)
+    const longURL = urlDatabase[req.params.shortURL];
+    res.redirect(urlDatabase[longURL])
   })
 
   app.post("/urls", (req, res) => {
     console.log(req.body);  
     res.redirect('/urls/:shortURL');         
   });
+
+  app.post("/urls/:shortURL/delete", (req, res) => {
+    const shortURL = req.params.shortURL
+    delete urlDatabase[shortURL];
+    res.redirect("/urls");
+  })
 
 app.listen(PORT, () => {
   console.log(`Tiny app listening on port ${PORT}!`);
